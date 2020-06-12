7e93410c8bcb96199c88028e25fb191331d6719e
Samara Grant <samara.amanda01@gmail.com>
2020-06-12 18:55:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -54,6 +54,6 @@ function switchAge(age) {
         console.log('You are a teenager');
         break;
     default:
-        console.log('You have an age')
+        console.log('You have an age');
 }
 }
